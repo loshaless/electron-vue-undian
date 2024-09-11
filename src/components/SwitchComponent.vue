63eda2d37c93e5a060a642b0e9a800a56647c006
Hansel Loshaless <hansel.loshaless@cimbniaga.co.id>
2024-09-11 20:54:59 +0700

    remove import defineProps and emits

--- src/components/SwitchComponent.vue
+++ src/components/SwitchComponent.vue
@@ -2,39 +2,42 @@
   <div
     :class="[
       'relative inline-flex items-center h-6 rounded-full w-11 transition-colors duration-300',
-      { 'bg-green-500': isChecked, 'bg-gray-300': !isChecked }
+      { 'bg-green-500': isChecked, 'bg-gray-300': !isChecked },
     ]"
     @click="toggle"
   >
     <span
       :class="[
         'inline-block w-4 h-4 transform bg-white rounded-full transition-transform duration-300',
-        { 'translate-x-6': isChecked, 'translate-x-1': !isChecked }
+        { 'translate-x-6': isChecked, 'translate-x-1': !isChecked },
       ]"
     ></span>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {ref, watch, defineProps, defineEmits} from 'vue';
+import { ref, watch } from "vue";
 
 const props = defineProps({
   value: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
 });
 
-const emit = defineEmits(['input']);
+const emit = defineEmits(["input"]);
 const isChecked = ref(props.value);
 
-watch(() => props.value, (newValue) => {
-  isChecked.value = newValue;
-});
+watch(
+  () => props.value,
+  (newValue) => {
+    isChecked.value = newValue;
+  }
+);
 
 function toggle() {
   isChecked.value = !isChecked.value;
-  emit('input', isChecked.value);
+  emit("input", isChecked.value);
 }
 </script>
 
