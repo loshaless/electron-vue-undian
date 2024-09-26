5147699446bd2c25b1d606d671beb011b291315f
hansel <hansel.loshaless@cimbniaga.co.id>
2024-09-26 16:18:32 +0700

    automatic roller

--- src/pages/RollerSettings.vue
+++ src/pages/RollerSettings.vue
@@ -1,18 +1,16 @@
 <script lang="ts" setup>
 import {computed, onMounted, reactive, Ref, ref} from "vue";
-import SwitchComponent from "../components/SwitchComponent.vue";
 import {IpcChannels} from "../constants/enum/IpcChannels";
 import SelectComponent from "../components/SelectComponent.vue";
 import LoadingComponent from "../components/LoadingComponent.vue";
-import {WinnerView} from "../constants/types/WinnerView";
 import {useCategory} from "../composables/useCategory";
 import {Prize} from "../constants/types/Prize";
 import {Category} from "../constants/types/Category";
 import {Quota} from "../constants/types/Quota";
 import ModalComponent from "../components/ModalComponent.vue";
-import {formatNumber} from "../utils/generalUtils";
+import {formatNumber, replaceSpaceWithUnderscore} from "../utils/generalUtils";
+import { WinnerView } from "../constants/types/WinnerView";
 
-const canControlRoller = ref(false)
 const isLoading = ref(false)
 
 /*  PRIZE  */
@@ -38,15 +36,17 @@ onMounted(() => {
 })
 
 /* SELECTED CATEGORY */
-const {listOfCategory, getCategory} = useCategory(prizeList);
+const {listOfCategory} = useCategory(prizeList);
 const selectedCategory = ref(1);
 
 const selectedCategoryData = computed(() => {
   const result = {
+    categoryId: 0,
     categoryName: '',
     minBalance: 0,
     totalWinner: 0,
     prize: [] as {
+      tableName: string,
       prizeName: string,
       regions: string[],
       numOfItem: number
@@ -55,14 +55,17 @@ const selectedCategoryData = computed(() => {
 
   const category: Category | undefined = listOfCategory.find((category) => category.id == selectedCategory.value);
   result.categoryName = category?.name ?? ''
+  result.categoryId = category?.id ?? 0
   result.minBalance = category?.minBalance ?? 0
 
   category?.prize?.forEach((prizeId: number) => {
     const prizeDetail = prizeList.value.find((p: Prize) => p.id === prizeId)
     prizeDetail?.detail.forEach((quota: Quota) => {
       result.totalWinner += +quota.numOfItem
+      const tableName = `customer_${replaceSpaceWithUnderscore(category.name)}_${replaceSpaceWithUnderscore(quota.name.join('_'))}`
 
       return result.prize.push({
+        tableName: tableName,
         prizeName: prizeDetail?.name,
         regions: quota.name,
         numOfItem: quota.numOfItem
@@ -78,41 +81,63 @@ const prizeDetailModalState = reactive({
   isOpen: false
 })
 
-/* START & STOP ROLLER */
-const listOfWinner: Ref<WinnerView[]> = ref([])
-const isRollerStart: Ref<Boolean> = ref(false)
-
 /* AUTOMATIC ROLLER */
 const rollAnimationTime = ref(1)
 const showWinnerTime = ref(1)
-const shouldContinueRolling = ref(true);
 const isAutomaticRollerStart = ref(false)
+
+/* ROLLER QUEUE */
+function generateRollerQueue() {
+  const result: any[] = []
+
+  selectedCategoryData.value.prize.forEach((prize) => {
+    for (let i = 0; i < prize.numOfItem; i++) {
+      result.push(
+        [prize.tableName, 
+          {
+            prizeName: prize.prizeName,
+            category: selectedCategoryData.value.categoryId,
+            rollId: 0,
+            winnerName: ''
+          } as WinnerView
+        ])
+    }
+  })
+  return result
+}
 
 async function startRollerWithoutStopper() {
   isAutomaticRollerStart.value = true
 
-  while (listOfWinner.value.length && shouldContinueRolling.value) {
-    moveRoller()
+  const rollerQueue = generateRollerQueue()
+  let index = 0
+  while (index < rollerQueue.length) {
+    moveRoller(rollerQueue[index][1], rollerQueue[index][0])
     await new Promise(resolve => setTimeout(resolve, rollAnimationTime.value * 1000))
 
-    if (shouldContinueRolling.value) {
-      stopRoller()
+    while(!winner.value) {
       await new Promise(resolve => setTimeout(resolve, showWinnerTime.value * 1000))
     }
+    stopRoller()
+    await new Promise(resolve => setTimeout(resolve, showWinnerTime.value * 1000))
+    index++
   }
   isAutomaticRollerStart.value = false
 }
 
-/* MANUAL ROLLER */
-function moveRoller() {
-  isRollerStart.value = true
+function moveRoller(winnerView: WinnerView, database: string) {
   window.ipcRenderer.send(IpcChannels.START_ROLLING)
+  window.ipcRenderer.send(IpcChannels.GET_A_WINNER, winnerView, database)
 }
 
+const winner = ref<WinnerView>()
+window.ipcRenderer.on(IpcChannels.GET_A_WINNER, (event, winnerView) => {
+  winner.value = winnerView
+})
+
 function stopRoller() {
-  isRollerStart.value = false
-  const winner = listOfWinner.value.shift()
-  window.ipcRenderer.send(IpcChannels.STOP_ROLLING, JSON.parse(JSON.stringify(winner)))
+  window.ipcRenderer.send(IpcChannels.STOP_ROLLING, JSON.parse(JSON.stringify(winner.value)))
+  winner.value = undefined
 }
 </script>
 
@@ -130,38 +155,14 @@ function stopRoller() {
             :options="listOfCategory"
           />
         </div>
-        <div class="flex gap-3 my-3">
-          <switch-component
-            v-model:value="canControlRoller"
-            @input="canControlRoller = $event"
-          />
-          <p>Can Control Roller ?</p>
-        </div>
 
         <!-- Loading when roller is start -->
         <loading-component v-if="isLoading" class="my-3"/>
 
-        <div v-else class="grid grid-cols-2 gap-3">
+        <div v-else class="grid grid-cols-2 gap-3 mt-5">
           <!-- Roller Control -->
           <div>
-            <div v-if="canControlRoller" class="flex gap-3 mt-4">
-              <button
-                v-if="!isRollerStart"
-                class="bg-green-500 hover:bg-green-300 p-2 rounded-md text-white"
-                @click="moveRoller()"
-              >
-                Start Roller
-              </button>
-              <button
-                v-else
-                class="bg-red-500 text-white p-2 rounded-md hover:bg-red-300"
-                @click="stopRoller(); shouldContinueRolling = false"
-              >
-                Stop and Set Winner
-              </button>
-            </div>
             <div
-              v-else-if="!isAutomaticRollerStart"
               class="mt-4 flex flex-col justify-end flex-1"
             >
               <div class="flex gap-3 items-center mb-3">
@@ -189,13 +190,13 @@ function stopRoller() {
 
               <button
                 v-if="showWinnerTime > 0 && rollAnimationTime > 0"
-                class="bg-green-500 hover:bg-green-300 p-2 rounded-md text-white"
+                class="bg-green-500 hover:bg-green-300 p-2 rounded-md text-white mt-3"
                 @click="startRollerWithoutStopper()"
               >
                 Start Roller Without Stopper
               </button>
             </div>
-            <loading-component v-else-if="isAutomaticRollerStart" class="my-3"/>
+            <loading-component v-if="isAutomaticRollerStart" class="my-3"/>
           </div>
 
           <!-- PRIZE DATA DETAIL -->
